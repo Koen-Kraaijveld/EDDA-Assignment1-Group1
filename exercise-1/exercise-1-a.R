82c9b861c0f97d2748a4d847dd5489abadce7ce8
UkeraGan <90500359+UkeraGan@users.noreply.github.com>
2023-02-16 20:03:04 +0100

    Update exercise-1-a.R

--- exercise-1/exercise-1-a.R
+++ exercise-1/exercise-1-a.R
@@ -1,33 +1,30 @@
+# Read data from file and extract birthweight column
 data = read.table("birthweight.txt", header=TRUE)
 birthweight = data$birthweight
 
-# normality check
+# Check for normality and compute Shapiro-Wilk test p-value
 qqnorm(birthweight)
 shapiro_pval = shapiro.test(birthweight)[[2]]
 hist(birthweight)
 cat(sprintf("Shapiro-Wilk test p-value: %f\n", shapiro_pval))
-# add hist, maybe boxplot but not necessary
 
-
-# bounded CI
+# Compute a bounded 96%-CI for the mean
 alpha = 0.04
 mean = mean(birthweight)
 s = sd(birthweight)
 n = length(birthweight)
 t_alpha = qt(1 - (alpha/2), df=n-1)
 error = t_alpha * (s / sqrt(n))
-
 lower_bound = mean - error
 upper_bound = mean + error
 ci = c(lower_bound, upper_bound)
 cat(sprintf("Bounded 96%%-CI: [%f, %f]\n", ci[1], ci[2]))
 
-
-# minimal sample size
+# Compute the minimum sample size for the error length to be 100
 min_n = ((t_alpha)^2 * s^2) / 50^2
 cat(sprintf("Minimum sample size for error length 100: %f\n", min_n))
 
-#bootstrap
+# Compute bootstrap 96%-CI for the mean and compare to the bounded CI
 B=1000
 Tstar=numeric(B)
 for(i in 1:B) {
