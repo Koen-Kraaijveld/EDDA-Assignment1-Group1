4ef3ab466f55be6fbd4d08e2be30ceae3b9cd6fd
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2023-02-17 15:38:45 +0100

    Added attempt for exercise-2-c

--- exercise-1/exercise-1-d.R
+++ exercise-1/exercise-1-d.R
@@ -1,3 +1,11 @@
-data = read.table("birthweight.txt", header=TRUE)
+data = read.table("exercise-1/birthweight.txt", header=TRUE)
 birthweight = data$birthweight
 
+print(mean(birthweight))
+print(pnorm(2600, mean=mean(birthweight), sd=sd(birthweight)))
+
+n = length(birthweight)
+p = sum(birthweight < 2600) / length(birthweight)
+print((0.3297872 - 0.25) / (sd(birthweight) / sqrt(n)))
+se = sd(birthweight) / sqrt(n)
+print(se)
