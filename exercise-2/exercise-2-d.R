4ce4ccce25ba5ee2e68a9dc8639c4a541e86383d
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2023-02-17 20:38:13 +0100

    Cleaned up code from exercise-2-d

--- exercise-2/exercise-2-d.R
+++ exercise-2/exercise-2-d.R
@@ -3,17 +3,29 @@ before = data$Before
 after = data$After8weeks
 
 n = length(after)
-t = max(after)
-B = 1000
-t_star = numeric(B)
-for (i in 1:B) {
-  x_star = runif(n, min=3, max=12)
-  t_star[i] = max(x_star)
+
+theta = numeric(9)
+p_val = numeric(9)
+
+for (i in 3:12) {
+  t = max(after)
+  B = 1000
+  t_star = numeric(B)
+  for (j in 1:B) {
+    x_star = runif(n, min=3, max=i)
+    t_star[j] = max(x_star)
+  }
+  
+  pl = sum(t_star < t) / B
+  pr = sum(t_star > t) / B
+  
+  theta[i-2] = i
+  p_val[i-2] = 2 * min(pl, pr)
 }
 
-pl = sum(t_star < t) / B
-pr = sum(t_star > t) / B
-print(2 * min(pl, pr))
+df = data.frame(
+  theta = theta,
+  p_value = p_val
+)
 
-par(mfrow=c(1,1))
-hist(t_star)
+print(df)
