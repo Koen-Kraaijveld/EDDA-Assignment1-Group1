4939aaa3b36ae2c57ee6dbca51c0ae2cbebf84bd
UkeraGan <90500359+UkeraGan@users.noreply.github.com>
2023-02-24 09:51:16 +0100

    no shuffle

--- exercise-4/exercise-4-a.R
+++ exercise-4/exercise-4-a.R
@@ -9,15 +9,15 @@ blocks <- rep(1:6, each = 4)
 
 # Randomly assign nitrogen to two plots in each block
 nitrogen <- rep(c(1,1,0,0), 6)
-nitrogen <- sample(nitrogen)
+#nitrogen <- sample(nitrogen)
 
 # Randomly assign phosphate to two plots in each block
 phosphate <- rep(c(1,0,1,0), 6)
-phosphate <- sample(phosphate)
+#phosphate <- sample(phosphate)
 
 # Assign potassium to  two plots in each block
 potassium <- rep(c(0,1,1,0), 6)
-potassium <- sample(potassium)
+#potassium <- sample(potassium)
 
 # Create a data frame to represent the randomization
 randomization <- data.frame(blocks, nitrogen, phosphate, potassium)
