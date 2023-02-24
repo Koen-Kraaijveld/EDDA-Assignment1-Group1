1fd6c19d1828312478f93ad506ae3aed1bc506e4
UkeraGan <90500359+UkeraGan@users.noreply.github.com>
2023-02-24 07:30:47 +0100

    exercise 4

--- exercise-4/exercise-4-a.R
+++ exercise-4/exercise-4-a.R
@@ -1,8 +1,8 @@
 # Load MASS package
 library(MASS)
 
-# Set seed for reproducibility
-set.seed(123)
+# Set seed 
+set.seed(0)
 
 # Create a vector representing the blocks
 blocks <- rep(1:6, each = 4)
@@ -24,3 +24,22 @@ randomization <- data.frame(blocks, nitrogen, phosphate, potassium)
 
 # View the resulting randomization
 randomization
+
+# Part B:
+
+# Average Yield
+avg_yield <- with(npk, tapply(yield, list(block, N), mean))
+
+# Create a bar plot to show the results
+barplot(avg_yield, beside = TRUE, legend = rownames(avg_yield),
+        xlab = "Block", ylab = "Average yield", main = "Effect of nitrogen on yield")
+
+
+# Part C:
+
+# A full two-way ANOVA model
+model <- aov(yield ~ block * N, data = npk)
+
+# Print the ANOVA table
+anova(model)
+
